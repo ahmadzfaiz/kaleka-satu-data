ad5ce88087df4ab2d638a6556808de33033f49b0
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-11-07 21:50:40 +0700

    Update Data

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+@admin.register(Testing)
+class TestingModel(admin.ModelAdmin):
+    search_fields = ('kode', 'nama')
+    list_filter = ('kode', 'nama', 'created_at', 'updated_at', 'user')
+    list_display = ('id', 'kode', 'nama', 'created_at', 'updated_at', 'user')
+
+@admin.register(Provinsi)
+class TestingModel(admin.ModelAdmin):
+    search_fields = ('kode', 'nama')
+    list_filter = ('kode', 'nama', 'created_at', 'updated_at', 'user')
+    list_display = ('id', 'kode', 'nama', 'created_at', 'updated_at', 'user')
--- data/models.py
+++ data/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Testing(models.Model):
+    class Meta:
+        verbose_name = 'Testing Data'
+        verbose_name_plural = 'Testing Data'
+
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    kode = models.CharField(max_length=5)
+    nama = models.CharField(max_length=30)
+    deskripsi = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nama
+
+class Provinsi(models.Model):
+    class Meta:
+        verbose_name = 'Provinsi'
+        verbose_name_plural = 'Provinsi'
+
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    kode = models.CharField(max_length=2)
+    nama = models.CharField(max_length=30)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nama
