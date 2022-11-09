8418dd8313bf6b7561541934d44f5810d10b3d12
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-11-09 16:38:40 +0700

    Update Alamat & Kode Pos

--- data/admin.py
+++ data/admin.py
@@ -7,9 +7,35 @@ class TestingModel(admin.ModelAdmin):
     search_fields = ('kode', 'nama')
     list_filter = ('kode', 'nama', 'created_at', 'updated_at', 'user')
     list_display = ('id', 'kode', 'nama', 'created_at', 'updated_at', 'user')
+    readonly_fields = ('user',)
 
-@admin.register(Provinsi)
-class TestingModel(admin.ModelAdmin):
-    search_fields = ('kode', 'nama')
-    list_filter = ('kode', 'nama', 'created_at', 'updated_at', 'user')
-    list_display = ('id', 'kode', 'nama', 'created_at', 'updated_at', 'user')
+    def save_model(self, request, obj, form, change): 
+        obj.user = request.user
+        obj.save()
+
+@admin.register(Alamat)
+class AlamatModel(admin.ModelAdmin):
+    search_fields = ('nama_prov', 'nama_kabkot', 'nama_kec', 'nama_desa')
+    list_filter = ('kode_prov', 'nama_prov', 
+        'kode_kabkot', 'nama_kabkot', 
+        'kode_kec', 'nama_kec',
+        'kode_desa', 'nama_desa',
+        'dasar_hukum', 'status_data',
+        'created_at', 'updated_at', 'user')
+    list_display = ('id', 'nama_prov', 'nama_kabkot', 'nama_kec', 'nama_desa', 'updated_at', 'user')
+    readonly_fields = ('user',)
+
+    def save_model(self, request, obj, form, change): 
+        obj.user = request.user
+        obj.save()
+
+@admin.register(KodePos)
+class KodePosModel(admin.ModelAdmin):
+    search_fields = ('kode',)
+    list_filter = ('kode', 'dasar_hukum', 'status_data', 'created_at', 'updated_at', 'user')
+    raw_id_fields = ('alamat',)
+    readonly_fields = ('user',)
+
+    def save_model(self, request, obj, form, change): 
+        obj.user = request.user
+        obj.save()
--- data/models.py
+++ data/models.py
@@ -19,14 +19,47 @@ class Testing(models.Model):
     def __str__(self):
         return self.nama
 
-class Provinsi(models.Model):
+class Alamat(models.Model):
     class Meta:
-        verbose_name = 'Provinsi'
-        verbose_name_plural = 'Provinsi'
+        verbose_name = 'Alamat'
+        verbose_name_plural = 'Alamat'
 
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-    kode = models.CharField(max_length=2)
-    nama = models.CharField(max_length=30)
+
+    kode_prov = models.CharField(max_length=2, verbose_name='Kode Provinsi')
+    nama_prov = models.CharField(max_length=30, verbose_name='Nama Provinsi', help_text="Tuliskan nama provinsi tanpa imbuhan 'Provinsi', 'Prov.' dsb. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
+    kode_kabkot = models.CharField(max_length=4, verbose_name='Kode Kabupaten/Kota')
+    nama_kabkot = models.CharField(max_length=30, verbose_name='Nama Kabupaten/Kota', help_text="Tuliskan nama kabupaten tanpa imbuhan 'Kabupaten', 'Kab.' dsb. Untuk nama kota, gunakan imbuhan 'Kota'. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
+    kode_kec = models.CharField(max_length=6, verbose_name='Kode Kecamatan')
+    nama_kec = models.CharField(max_length=30, verbose_name='Nama Kecamatan', help_text="Tuliskan nama kecamatan tanpa imbuhan 'Kecamatan', 'Kec.' dsb. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
+    kode_desa = models.CharField(max_length=10, verbose_name='Kode Desa/Kelurahan')
+    nama_desa = models.CharField(max_length=30, verbose_name='Nama Desa/Kelurahan', help_text="Tuliskan nama desa tanpa imbuhan 'Desa', 'Ds.' dsb. Untuk nama kelurahan, gunakan imbuhan 'Kel.'. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
+    dasar_hukum = models.CharField(max_length=30, verbose_name='Dasar Hukum')
+    status_data = models.CharField(max_length=11, verbose_name='Status Data', choices=[('Updated', 'Updated'), ('Depreciated', 'Depreciated')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nama
+    
+    def save(self, *args, **kwargs):
+        self.nama_prov = self.nama_prov.upper()
+        self.nama_kabkot = self.nama_kabkot.upper()
+        self.nama_kec = self.nama_kec.upper()
+        self.nama_desa = self.nama_desa.upper()
+        return super(Alamat, self).save(*args, **kwargs)
+
+class KodePos(models.Model):
+    class Meta:
+        verbose_name = 'Kode Pos'
+        verbose_name_plural = 'Kode Pos'
+    
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    kode = models.CharField(max_length=5)
+    alamat = models.ForeignKey(Alamat, on_delete=models.CASCADE)
+    dasar_hukum = models.CharField(max_length=30, verbose_name='Dasar Hukum')
+    status_data = models.CharField(max_length=11, verbose_name='Status Data', choices=[('Updated', 'Updated'), ('Depreciated', 'Depreciated')])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
