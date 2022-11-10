d559c2f5336f7c4159a67d6f9cd4ce97ce63226a
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-11-10 17:13:27 +0700

    Update Profesi, KBJI & Orang

--- data/admin.py
+++ data/admin.py
@@ -23,7 +23,13 @@ class AlamatModel(admin.ModelAdmin):
         'dasar_hukum', 'status_data',
         'created_at', 'updated_at', 'user')
     list_display = ('id', 'nama_prov', 'nama_kabkot', 'nama_kec', 'nama_desa', 'updated_at', 'user')
-    readonly_fields = ('user',)
+    fieldsets = [
+        ('Provinsi', {'fields': ('kode_prov', 'nama_prov')}),
+        ('Kabupaten/Kota', {'fields': ('kode_kabkot', 'nama_kabkot')}),
+        ('Kecamatan', {'fields': ('kode_kec', 'nama_kec')}),
+        ('Desa/Kelurahan', {'fields': ('kode_desa', 'nama_desa')}),
+        ('Metadata', {'fields': ('dasar_hukum', 'status_data')})
+    ]
 
     def save_model(self, request, obj, form, change): 
         obj.user = request.user
@@ -33,6 +39,46 @@ class AlamatModel(admin.ModelAdmin):
 class KodePosModel(admin.ModelAdmin):
     search_fields = ('kode',)
     list_filter = ('kode', 'dasar_hukum', 'status_data', 'created_at', 'updated_at', 'user')
+    raw_id_fields = ('alamat',)
+    readonly_fields = ('user',)
+
+    def save_model(self, request, obj, form, change): 
+        obj.user = request.user
+        obj.save()
+
+@admin.register(KBJI)
+class KBJIModel(admin.ModelAdmin):
+    search_fields = ('golongan_pokok', 'subgolongan_pokok', 'golongan', 'subgolongan', 'jabatan')
+    list_filter = ('kode_gol_pokok', 'kode_subgol_pokok', 'kode_gol', 'kode_subgol', 'kode_jabatan', 'dasar_hukum', 'status_data', 'created_at', 'updated_at', 'user')
+    fieldsets = [
+        ('Golongan Pokok', {'fields': ('golongan_pokok', 'kode_gol_pokok')}),
+        ('Sub-Golongan Pokok', {'fields': ('subgolongan_pokok', 'kode_subgol_pokok')}),
+        ('Golongan', {'fields': ('golongan', 'kode_gol')}),
+        ('Sub-Golongan', {'fields': ('subgolongan', 'kode_subgol')}),
+        ('Jabatan', {'fields': ('jabatan', 'kode_jabatan')}),
+        ('Metadata', {'fields': ('dasar_hukum', 'status_data')})
+    ]
+
+    def save_model(self, request, obj, form, change): 
+        obj.user = request.user
+        obj.save()
+
+@admin.register(Profesi)
+class ProfesiModel(admin.ModelAdmin):
+    search_fields = ('nama',)
+    list_filter = ('nama', 'kode_kbji', 'created_at', 'updated_at', 'user')
+    raw_id_fields = ('kode_kbji',)
+    readonly_fields = ('user',)
+
+    def save_model(self, request, obj, form, change): 
+        obj.user = request.user
+        obj.save()
+
+@admin.register(Orang)
+class OrangModel(admin.ModelAdmin):
+    search_fields = ('nama_lengkap',)
+    list_filter = ('nik', 'nama_lengkap', 'jenis_kelamin', 'tempat_lahir', 'tanggal_lahir', 'status_kawin', 'profesi', 'rt', 'rw', 'alamat', 'created_at', 'updated_at', 'user')
+    filter_horizontal = ('profesi',)
     raw_id_fields = ('alamat',)
     readonly_fields = ('user',)
 
--- data/models.py
+++ data/models.py
@@ -25,14 +25,13 @@ class Alamat(models.Model):
         verbose_name_plural = 'Alamat'
 
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
-
-    kode_prov = models.CharField(max_length=2, verbose_name='Kode Provinsi')
+    kode_prov = models.CharField(max_length=2, unique=True, verbose_name='Kode Provinsi')
     nama_prov = models.CharField(max_length=30, verbose_name='Nama Provinsi', help_text="Tuliskan nama provinsi tanpa imbuhan 'Provinsi', 'Prov.' dsb. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
-    kode_kabkot = models.CharField(max_length=4, verbose_name='Kode Kabupaten/Kota')
+    kode_kabkot = models.CharField(max_length=4, unique=True, verbose_name='Kode Kabupaten/Kota')
     nama_kabkot = models.CharField(max_length=30, verbose_name='Nama Kabupaten/Kota', help_text="Tuliskan nama kabupaten tanpa imbuhan 'Kabupaten', 'Kab.' dsb. Untuk nama kota, gunakan imbuhan 'Kota'. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
-    kode_kec = models.CharField(max_length=6, verbose_name='Kode Kecamatan')
+    kode_kec = models.CharField(max_length=6, unique=True, verbose_name='Kode Kecamatan')
     nama_kec = models.CharField(max_length=30, verbose_name='Nama Kecamatan', help_text="Tuliskan nama kecamatan tanpa imbuhan 'Kecamatan', 'Kec.' dsb. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
-    kode_desa = models.CharField(max_length=10, verbose_name='Kode Desa/Kelurahan')
+    kode_desa = models.CharField(max_length=10, unique=True, verbose_name='Kode Desa/Kelurahan')
     nama_desa = models.CharField(max_length=30, verbose_name='Nama Desa/Kelurahan', help_text="Tuliskan nama desa tanpa imbuhan 'Desa', 'Ds.' dsb. Untuk nama kelurahan, gunakan imbuhan 'Kel.'. Tulisan akan otomatis terkonversi menjadi huruf kapital.")
     dasar_hukum = models.CharField(max_length=30, verbose_name='Dasar Hukum')
     status_data = models.CharField(max_length=11, verbose_name='Status Data', choices=[('Updated', 'Updated'), ('Depreciated', 'Depreciated')])
@@ -41,7 +40,7 @@ class Alamat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.nama
+        return self.nama_desa
     
     def save(self, *args, **kwargs):
         self.nama_prov = self.nama_prov.upper()
@@ -65,4 +64,71 @@ class KodePos(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
+        return self.kode
+
+class KBJI(models.Model):
+    class Meta:
+        verbose_name = 'Klasifikasi Baku Jabatan'
+        verbose_name_plural = 'Klasifikasi Baku Jabatan'
+
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    kode_gol_pokok = models.PositiveSmallIntegerField(unique=True, verbose_name='Kode Golongan Pokok')
+    golongan_pokok = models.CharField(max_length=50, verbose_name='Nama Golongan Pokok', help_text='Tulisan akan otomatis terkonversi menjadi huruf kapital.')
+    kode_subgol_pokok = models.PositiveSmallIntegerField(unique=True, verbose_name='Kode Sub-Golongan Pokok')
+    subgolongan_pokok = models.CharField(max_length=50, verbose_name='Nama Sub-Golongan Pokok', help_text='Tulisan akan otomatis terkonversi menjadi huruf kapital.')
+    kode_gol = models.PositiveSmallIntegerField(unique=True, verbose_name='Kode Golongan')
+    golongan = models.CharField(max_length=50, verbose_name='Nama Golongan', help_text='Tulisan akan otomatis terkonversi menjadi huruf kapital.')
+    kode_subgol = models.PositiveSmallIntegerField(unique=True, verbose_name='Kode Sub-Golongan')
+    subgolongan = models.CharField(max_length=50, verbose_name='Nama Sub-Golongan', help_text='Tulisan akan otomatis terkonversi menjadi huruf kapital.')
+    kode_jabatan = models.DecimalField(max_digits=6, decimal_places=2, verbose_name='Kode Jabatan')
+    jabatan = models.CharField(max_length=50, verbose_name='Nama Jabatan', help_text='Tulisan akan otomatis terkonversi menjadi huruf kapital.')
+    dasar_hukum = models.CharField(max_length=30)
+    status_data = models.CharField(max_length=11, verbose_name='Status Data', choices=[('Updated', 'Updated'), ('Depreciated', 'Depreciated')])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.jabatan
+
+class Profesi(models.Model):
+    class Meta:
+        verbose_name = 'Profesi'
+        verbose_name_plural = 'Profesi'
+
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    nama = models.CharField(max_length=50)
+    kode_kbji = models.ForeignKey(KBJI, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
         return self.nama
+
+class Orang(models.Model):
+    class Meta:
+        verbose_name = 'Orang'
+        verbose_name_plural = 'Orang'
+
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
+    nik = models.PositiveBigIntegerField(verbose_name='Nomor Induk Kependudukan', unique=True)
+    nama_lengkap = models.CharField(max_length=50, help_text='Nama sesuai KTP, tidak ditambahkan gelar.')
+    jenis_kelamin = models.CharField(max_length=9, choices=[('Laki-laki', 'Laki-laki'), ('Perempuan', 'Perempuan')])
+    tempat_lahir = models.CharField(max_length=30, help_text='Tulisan akan otomatis terkonversi menjadi huruf kapital.')
+    tanggal_lahir = models.DateField()
+    status_kawin = models.CharField(max_length=11, choices=[('Belum Kawin', 'Belum Kawin'), ('Kawin', 'Kawin'), ('Cerai Hidup', 'Cerai Hidup'), ('Cerai Mati', 'Cerai Mati')])
+    profesi = models.ManyToManyField(Profesi)
+    rt = models.PositiveSmallIntegerField(verbose_name='RT')
+    rw = models.PositiveSmallIntegerField(verbose_name='RW')
+    alamat = models.ForeignKey(Alamat, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nama_lengkap
+
+    def save(self, *args, **kwargs):
+        self.tempat_lahir = self.tempat_lahir.upper()
+        return super(Orang, self).save(*args, **kwargs)
