567629a4980cf39ceb4d2cf0ac6d51ea10d46ce2
Ahmad Zaenun Faiz <75885530+ahmadzfaiz@users.noreply.github.com>
2022-12-01 19:47:08 +0700

    Dataset Negara

--- data/dataset/iucn.py
+++ data/dataset/iucn.py
@@ -3,6 +3,7 @@ from django.db import models
 import uuid
 from django.contrib.auth.models import User
 from .spesies import Spesies
+from .negara import Negara
 
 class IUCN(models.Model):
     class Meta:
@@ -26,23 +27,19 @@ class IUCN(models.Model):
     )
     tanggal_asesmen = models.DateField()
     referensi_asesmen = models.TextField()
-    habitat_asal = models.CharField(max_length=50)
+    habitat_asal = models.ManyToManyField(Negara)
     lokasi_geografis = models.TextField(blank=True, help_text='Saat ini tolong diisikan dengan koordinat Well-known Text (WKT) Geografis WGS84. Ke depan akan diganti dengan fitur digitasi spasial')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        self.habitat_asal = self.habitat_asal.upper()
-        return super(IUCN, self).save(*args, **kwargs)
-
 
 @admin.register(IUCN)
 class IUCNModel(admin.ModelAdmin):
     search_fields = ('spesies',)
     ordering = ('spesies',)
     list_filter = ('kategori', 'tanggal_asesmen', 'referensi_asesmen', 'habitat_asal', 'created_at', 'updated_at', 'user')
-    list_display = ('id', 'spesies', 'kategori', 'tanggal_asesmen', 'referensi_asesmen', 'habitat_asal', 'updated_at', 'user')
+    list_display = ('id', 'spesies', 'kategori', 'tanggal_asesmen', 'referensi_asesmen', 'updated_at', 'user')
+    filter_horizontal = ('habitat_asal',)
     raw_id_fields = ('spesies',)
     readonly_fields = ('user',)
 
